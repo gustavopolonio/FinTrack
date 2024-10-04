55707ced44dc41f1931f977969c2263c60eee380
Gustavo <gustavopolonio1@gmail.com>
2024-10-03 21:38:10 -0300

    feat[GET]: retrieve user transactions

--- backend/src/models/transactionModel.ts
+++ backend/src/models/transactionModel.ts
@@ -29,4 +29,6 @@ const transactionSchema = new Schema({
   timestamps: true
 })
 
+transactionSchema.index({ userId: 1 })
+
 export const Transaction = model('Transaction', transactionSchema)
--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,7 +1,10 @@
 import { FastifyInstance } from "fastify";
 import { ZodTypeProvider } from "fastify-type-provider-zod";
 import { z } from "zod";
+import { Types } from "mongoose";
+
 import { User } from "../models/userModel";
+import { Transaction } from "../models/transactionModel";
 
 export async function userRoutes(server: FastifyInstance) {
   server.withTypeProvider<ZodTypeProvider>().post(
@@ -28,6 +31,39 @@ export async function userRoutes(server: FastifyInstance) {
         }
       } catch (err: any) {
         console.log(`Error creating new user: ${err}`)
+        throw new Error(err)
+      }
+    }
+  )
+
+  server.withTypeProvider<ZodTypeProvider>().get(
+    '/users/:userId/transactions',
+    {
+      schema: {
+        params: z.object({
+          userId: z.string().refine(value => Types.ObjectId.isValid(value), {
+            message: 'Invalid userId'
+          }),
+        })
+      }
+    },
+    async (request) => {
+      const { userId } = request.params
+
+      const user = await User.findById(userId)
+
+      if (!user) {
+        throw new Error('User not found!')
+      }
+
+      try {
+        const transactions = await Transaction.find({
+          userId
+        })
+
+        return { transactions }
+      } catch (err: any) {
+        console.log(`Error retrieving transactions of user: ${err}`)
         throw new Error(err)
       }
     }
